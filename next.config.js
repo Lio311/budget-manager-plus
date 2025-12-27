1678bf1ccb4ad253f508392338c57e7a25aa1ac4
Lior <lior31197@gmail.com>
2025-12-27 18:10:13 +0200

    fix: update CSP to allow clerk.keseflow.com

--- next.config.js
+++ next.config.js
@@ -38,7 +38,7 @@ const nextConfig = {
                     },
                     {
                         key: 'Content-Security-Policy',
-                        value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://clerk.budget-manager-plus.com https://*.clerk.accounts.dev https://va.vercel-scripts.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' blob: data: https://img.clerk.com https://images.clerk.dev; connect-src 'self' https://clerk.budget-manager-plus.com https://*.clerk.accounts.dev https://*.vercel-scripts.com;"
+                        value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://clerk.budget-manager-plus.com https://clerk.keseflow.com https://*.clerk.accounts.dev https://va.vercel-scripts.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' blob: data: https://img.clerk.com https://images.clerk.dev; connect-src 'self' https://clerk.budget-manager-plus.com https://clerk.keseflow.com https://*.clerk.accounts.dev https://*.vercel-scripts.com;"
                     }
                 ]
             }
