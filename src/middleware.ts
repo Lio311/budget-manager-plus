9ca21140c294b68b58de522a0434a6b927572e2f
Lior <lior31197@gmail.com>
2026-01-03 01:04:21 +0200

    Make personal and business landing pages public

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,8 @@ import { NextResponse } from 'next/server';
 
 const isPublicRoute = createRouteMatcher([
     '/',
+    '/personal(.*)',
+    '/business(.*)',
     '/landing_page(.*)',
     '/sign-in(.*)',
     '/sign-up(.*)',
